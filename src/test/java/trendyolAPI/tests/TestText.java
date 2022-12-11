cae8d40ca9c30481a15476ef455e4acb8b6c8865
Burslu <iberkayuslu41@gmail.com>
2022-12-11 16:42:01 +0300

    son hali BookStore

--- src/test/java/trendyolAPI/tests/TestText.java
+++ src/test/java/trendyolAPI/tests/TestText.java
@@ -31,8 +31,8 @@ public class TestText {
         Response response =
                 Helper.TrendyolGetHelperText(GetUrl,1,200);
         TextPojo textPojo = response.as(TextPojo.class);
-        Assert.assertEquals(textPojo.getData().get(0).getContent().length(),200);
-
+        //Assert.assertEquals(textPojo.getData().get(0).getContent().length(),200);
+        Assert.assertTrue(195 <= textPojo.getData().get(0).getContent().length() && textPojo.getData().get(0).getContent().length() <= 200);
 }
 @Test(description = "content deger 500 icin test gerceklestirildiginde 495 ile 500 arasinda bir contentlenght uzunlu almaliyiz . ")
     public void contentLenght500(){
@@ -40,7 +40,8 @@ public class TestText {
         Response response =
                 Helper.TrendyolGetHelperText(GetUrl,1,500);
         TextPojo textPojo = response.as(TextPojo.class);
-        Assert.assertEquals(textPojo.getData().get(0).getContent().length(),500);
+       // Assert.assertEquals(textPojo.getData().get(0).getContent().length(),500);
+    Assert.assertTrue(495 <= textPojo.getData().get(0).getContent().length() && textPojo.getData().get(0).getContent().length() <= 500);
 
     }
     @Test(description = "0 degeri verildiginde default deger olur 200 length uzunlugunda bir content dondurur (195,200 arasinda) ")
