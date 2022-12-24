204cffbcffc8ed4adb7d364107f49db212f18c1c
Burslu <iberkayuslu41@gmail.com>
2022-12-24 18:22:14 +0300

    changes made

--- src/test/java/trendyolAPI/tests/TestText.java
+++ src/test/java/trendyolAPI/tests/TestText.java
@@ -9,50 +9,50 @@ import trendyolAPI.pojo.TextPojo;
 
 public class TestText {
 
-//    @Test
-//
-//    public void Text(){
-//
-//        String GetUrl = "https://fakerapi.it/api/v1/texts";
-//
-//        Response response =
-//                Helper.TrendyolGetHelperText(GetUrl,1,500);
-//        TextPojo textPojo = response.as(TextPojo.class);
-//        Assert.assertEquals(textPojo.getData().size(),1);
-//        Assert.assertNotNull(textPojo.getData().get(0).getTitle());
-//        Assert.assertNotNull(textPojo.getData().get(0).getAuthor());
-//        Assert.assertNotNull(textPojo.getData().get(0).getGenre());
-//        Assert.assertNotNull(textPojo.getData().get(0).getContent());
-//    }
-//    @Test(description = "195 den buyuk olup 200den kucuk olan durumlar icin gecerli test . ")
-//
-//    public void contentLenght200(){
-//        String GetUrl = "https://fakerapi.it/api/v1/texts";
-//        Response response =
-//                Helper.TrendyolGetHelperText(GetUrl,1,200);
-//        TextPojo textPojo = response.as(TextPojo.class);
-//        //Assert.assertEquals(textPojo.getData().get(0).getContent().length(),200);
-//        Assert.assertTrue(195 <= textPojo.getData().get(0).getContent().length() && textPojo.getData().get(0).getContent().length() <= 200);
-//}
-//@Test(description = "content deger 500 icin test gerceklestirildiginde 495 ile 500 arasinda bir contentlenght uzunlu almaliyiz . ")
-//    public void contentLenght500(){
-//        String GetUrl = "https://fakerapi.it/api/v1/texts";
-//        Response response =
-//                Helper.TrendyolGetHelperText(GetUrl,1,500);
-//        TextPojo textPojo = response.as(TextPojo.class);
-//       // Assert.assertEquals(textPojo.getData().get(0).getContent().length(),500);
-//    Assert.assertTrue(495 <= textPojo.getData().get(0).getContent().length() && textPojo.getData().get(0).getContent().length() <= 500);
-//
-//    }
-//    @Test(description = "0 degeri verildiginde default deger olur 200 length uzunlugunda bir content dondurur (195,200 arasinda) ")
-//    public void contentLenght0(){
-//        String GetUrl = "https://fakerapi.it/api/v1/texts";
-//        Response response =
-//                Helper.TrendyolGetHelperText(GetUrl,1,0);
-//        TextPojo textPojo = response.as(TextPojo.class);
-//        System.out.println(textPojo.getData().get(0).getContent().length());
-//        Assert.assertTrue(195 <= textPojo.getData().get(0).getContent().length() && textPojo.getData().get(0).getContent().length() <= 200);
-//
-//    }
+    @Test
+
+    public void Text(){
+
+        String GetUrl = "https://fakerapi.it/api/v1/texts";
+
+        Response response =
+                Helper.TrendyolGetHelperText(GetUrl,1,500);
+        TextPojo textPojo = response.as(TextPojo.class);
+        Assert.assertEquals(textPojo.getData().size(),1);
+        Assert.assertNotNull(textPojo.getData().get(0).getTitle());
+        Assert.assertNotNull(textPojo.getData().get(0).getAuthor());
+        Assert.assertNotNull(textPojo.getData().get(0).getGenre());
+        Assert.assertNotNull(textPojo.getData().get(0).getContent());
+    }
+    @Test(description = "195 den buyuk olup 200den kucuk olan durumlar icin gecerli test . ")
+
+    public void contentLenght200(){
+        String GetUrl = "https://fakerapi.it/api/v1/texts";
+        Response response =
+                Helper.TrendyolGetHelperText(GetUrl,1,200);
+        TextPojo textPojo = response.as(TextPojo.class);
+        //Assert.assertEquals(textPojo.getData().get(0).getContent().length(),200);
+        Assert.assertTrue(195 <= textPojo.getData().get(0).getContent().length() && textPojo.getData().get(0).getContent().length() <= 200);
+}
+@Test(description = "content deger 500 icin test gerceklestirildiginde 495 ile 500 arasinda bir contentlenght uzunlu almaliyiz . ")
+    public void contentLenght500(){
+        String GetUrl = "https://fakerapi.it/api/v1/texts";
+        Response response =
+                Helper.TrendyolGetHelperText(GetUrl,1,500);
+        TextPojo textPojo = response.as(TextPojo.class);
+       // Assert.assertEquals(textPojo.getData().get(0).getContent().length(),500);
+    Assert.assertTrue(495 <= textPojo.getData().get(0).getContent().length() && textPojo.getData().get(0).getContent().length() <= 500);
+
+    }
+    @Test(description = "0 degeri verildiginde default deger olur 200 length uzunlugunda bir content dondurur (195,200 arasinda) ")
+    public void contentLenght0(){
+        String GetUrl = "https://fakerapi.it/api/v1/texts";
+        Response response =
+                Helper.TrendyolGetHelperText(GetUrl,1,0);
+        TextPojo textPojo = response.as(TextPojo.class);
+        System.out.println(textPojo.getData().get(0).getContent().length());
+        Assert.assertTrue(195 <= textPojo.getData().get(0).getContent().length() && textPojo.getData().get(0).getContent().length() <= 200);
+
+    }
 
 }
--- src/test/java/trendyolAPI/tests/quantityTest.java
+++ src/test/java/trendyolAPI/tests/quantityTest.java
@@ -9,55 +9,55 @@ import trendyolAPI.pojo.ResponseTrendyol;
 
 public class quantityTest {
 
-//    //Quantity alaninin calisip calismadigini kontrol eden test
-//    @Test(description = "0 in altinda bir deger girildiginde bos liste donmelidir")
-//    public void a(){
-//        String GetUrl = "https://fakerapi.it/api/v1/companies";
-//        Response response =
-//                Helper.TrendyolGetHelper(GetUrl,-10);
-//        ResponseTrendyol responseTrendyol = response.as(ResponseTrendyol.class);
-//        Assert.assertEquals(responseTrendyol.data.size(),0);
-//
-//
-//
-//    }
-//    @Test(description ="0 girildiginde Default deger doner ." )
-//    public void b(){
-//        String GetUrl = "https://fakerapi.it/api/v1/companies";
-//        Response response =
-//                Helper.TrendyolGetHelper(GetUrl,0);
-//        ResponseTrendyol responseTrendyol = response.as(ResponseTrendyol.class);
-//        Assert.assertEquals(responseTrendyol.data.size(),10);
-//
-//    }
-//    @Test(description =" 0 ile 1000 arasinda bir deger girildiginde girilen sayi kadar liste donmelidir")
-//    public void c(){
-//        String GetUrl = "https://fakerapi.it/api/v1/companies";
-//        Response response =
-//                Helper.TrendyolGetHelper(GetUrl,10);
-//        ResponseTrendyol responseTrendyol = response.as(ResponseTrendyol.class);
-//        Assert.assertEquals(responseTrendyol.getData().size(),10);
-//        Assert.assertEquals(responseTrendyol.total,10);
-//
-//    }
-//    @Test(description = " 1000 girildiginde 1000 kayitli liste donmelidir")
-//    public void d(){
-//        String GetUrl = "https://fakerapi.it/api/v1/companies";
-//        Response response =
-//                Helper.TrendyolGetHelper(GetUrl,1000);
-//        ResponseTrendyol responseTrendyol = response.as(ResponseTrendyol.class);
-//        Assert.assertEquals(responseTrendyol.getData().get(999).getId(),1000);
-//
-//    }
-//
-//
-//
-//    @Test(description =  "1000 uzeride bir deger girildiginde 1000 kayitli liste donmelidir")
-//    public void e(){
-//        String GetUrl = "https://fakerapi.it/api/v1/companies";
-//        Response response =
-//                Helper.TrendyolGetHelper(GetUrl,1005);
-//        ResponseTrendyol responseTrendyol = response.as(ResponseTrendyol.class);
-//        Assert.assertEquals(responseTrendyol.getData().get(999).getId(),1000);
-//    }
+    //Quantity alaninin calisip calismadigini kontrol eden test
+    @Test(description = "0 in altinda bir deger girildiginde bos liste donmelidir")
+    public void a(){
+        String GetUrl = "https://fakerapi.it/api/v1/companies";
+        Response response =
+                Helper.TrendyolGetHelper(GetUrl,-10);
+        ResponseTrendyol responseTrendyol = response.as(ResponseTrendyol.class);
+        Assert.assertEquals(responseTrendyol.data.size(),0);
+
+
+
+    }
+    @Test(description ="0 girildiginde Default deger doner ." )
+    public void b(){
+        String GetUrl = "https://fakerapi.it/api/v1/companies";
+        Response response =
+                Helper.TrendyolGetHelper(GetUrl,0);
+        ResponseTrendyol responseTrendyol = response.as(ResponseTrendyol.class);
+        Assert.assertEquals(responseTrendyol.data.size(),10);
+
+    }
+    @Test(description =" 0 ile 1000 arasinda bir deger girildiginde girilen sayi kadar liste donmelidir")
+    public void c(){
+        String GetUrl = "https://fakerapi.it/api/v1/companies";
+        Response response =
+                Helper.TrendyolGetHelper(GetUrl,10);
+        ResponseTrendyol responseTrendyol = response.as(ResponseTrendyol.class);
+        Assert.assertEquals(responseTrendyol.getData().size(),10);
+        Assert.assertEquals(responseTrendyol.total,10);
+
+    }
+    @Test(description = " 1000 girildiginde 1000 kayitli liste donmelidir")
+    public void d(){
+        String GetUrl = "https://fakerapi.it/api/v1/companies";
+        Response response =
+                Helper.TrendyolGetHelper(GetUrl,1000);
+        ResponseTrendyol responseTrendyol = response.as(ResponseTrendyol.class);
+        Assert.assertEquals(responseTrendyol.getData().get(999).getId(),1000);
+
+    }
+
+
+
+    @Test(description =  "1000 uzeride bir deger girildiginde 1000 kayitli liste donmelidir")
+    public void e(){
+        String GetUrl = "https://fakerapi.it/api/v1/companies";
+        Response response =
+                Helper.TrendyolGetHelper(GetUrl,1005);
+        ResponseTrendyol responseTrendyol = response.as(ResponseTrendyol.class);
+        Assert.assertEquals(responseTrendyol.getData().get(999).getId(),1000);
+    }
 }
